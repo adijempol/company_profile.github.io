9141a5ba9fb89ced583b47973221d9cf6f92e9b8
adijempol <83076262+adijempol@users.noreply.github.com>
2022-09-09 23:22:15 -0600

    upload

---
+++ js/src.js
@@ -0,0 +1,22 @@
+const menu = document.getElementById('toggle');
+const about = document.getElementById('about');
+const nav = document.getElementById('navbar');
+const oi = document.getElementById('oi');
+
+// console.log(oi);
+oi.addEventListener('click', function(){
+     console.log('ooi');
+          // navbar.classList.add('scolse');
+})
+// document.onclick = function(e){
+//      if(e.target.id !== 'navbar' && e.target.id !== 'toggle')
+//      {
+//           toggle.classList.remove('active');
+//           navbar.classList.remove('active');
+//      }
+// };
+
+// toggle.onclick = function(){
+//      toggle.classList.target('active');
+//      navbar.classList.target('active');
+// };
